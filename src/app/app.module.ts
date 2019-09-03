e46c6f15371619ed58468cd8ae470abb0ef03331
mounisha25 <mounisha.gelli@ggktech.com>
2019-09-03 11:46:10 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule }     from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { cartReducer } from './cart.reducer';
 import { MyCartComponent } from './my-cart/my-cart.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [AppComponent, MyCartComponent],
+  declarations: [AppComponent, MyCartComponent, ProductsComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({ count: cartReducer }),
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
